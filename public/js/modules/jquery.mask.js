f758f6e653041330724e82dd22e58402d0209dbd
Ilnur Khalilov <xomyaq@gmail.com>
2013-11-26 01:00:16 +0600

    Something near working version

--- public/js/modules/jquery.mask.js
+++ public/js/modules/jquery.mask.js
@@ -1,13 +1,41 @@
-(function ($) {
+;(function ($) {
   "use strict";
 
   var Mask = function (element, options) {
+    var $element = $(element);
+
     this.element = element;
+    this.$element = $element;
+    this.pattern = new RegExp($element.attr("pattern"));
+    this.mask = $element.data().mask;
     this.options = options;
+    this.value = "";
     this.init();
   };
 
-  Mask.prototype.init = function () {};
+  Mask.prototype.init = function () {
+    var that = this;
+
+    this.$element.on("keyup.mask", function (e) {
+      var value = this.value
+        , length = this.value.length
+        , match = function () { return value.match(/\-/g) };
+
+      that.value = value;
+
+      length === that.mask.indexOf("-", length)
+        && (!match() || (match() && match().length !== 2))
+        && (e.keyCode !== 8)
+        && (this.value += "-");
+
+      // Set focus to the next input
+      length === that.mask.length && !that.$element.hasClass("error") && that.next();
+    });
+  };
+
+  Mask.prototype.next = function () {
+    $("[tabindex=" + (parseInt(this.$element.attr("tabindex")) + 1) + "]")[0].focus();
+  };
 
   $.fn.mask = function (options) {
     return this.each(function () {
--- public/js/modules/main.js
+++ public/js/modules/main.js
@@ -1,9 +1,76 @@
-(function (window, document, $) {
+;(function (window, document, $) {
   "use strict";
 
-  $(".js-mask").mask();
+  var $document = $(document)
 
-  $(document).on("submit", ".js-pay-form", function (e) {
+  /**
+   * Checks input data by input pattern
+   * @param  {Object}  $input Input element jQuery wrapped
+   * @return {boolean}        Check result
+   */
+  var checkPattern = function ($input) {
+    return (new RegExp($input.attr("pattern"))).test($input.val());
+  };
+
+  /**
+   * Checks money input by pattern and compare with 1
+   * @param  {Object}  $input Input element jQuery wrapped
+   * @return {boolean}        Check result
+   */
+  var checkMoney = function ($input) {
+    return (parseInt($input.val()) >= 1) && checkPattern($input);
+  };
+
+  /**
+   * Checks field by given function
+   * @param {Function} fn Input check function
+   */
+  var checkField = function (fn) {
+    var $this = $(this);
+
+    fn($this)
+      ? $this.removeClass("error").addClass("correct")
+      : $this.removeClass("correct").addClass("error");
+  };
+
+  /**
+   * Send form data to server
+   * @param {Object} e Send event
+   */
+  var sendData = function (e) {
+    var formData = {
+      tel: $(".js-country-code").text() + $(".js-phone-code").val() + $(".js-phone-number").val().replace(/\-/g, ""),
+      amount: $(".js-money-amount").val()
+    };
+
     e.preventDefault();
+
+    console.log(formData);
+  };
+
+  // Set mask to phone input field
+  $(".js-phone-code, .js-phone-number").mask();
+
+  // Subscribe to events
+  $document.on("submit", ".js-pay-form", sendData);
+
+  $document.on("blur", ".js-check", function () {
+    checkField.call(this, checkPattern);
+  });
+
+  $document.on("focus", ".js-check", function () {
+    $(this).removeClass("error correct");
+  });
+
+  $document.on("keyup", ".js-form-unlock", function () {
+    checkField.call(this, checkMoney);
+  });
+
+  $document.on("keyup", ".js-check", function () {
+    if (checkPattern($(this)) && ($(".js-check").length === $(".correct").length)) {
+      $(".js-form-lock").removeClass("button-send_disabled").removeAttr("disabled");
+    } else {
+      $(".js-form-lock").addClass("button-send_disabled").attr("disabled", "disabled");
+    }
   });
 }(window, document, jQuery));
